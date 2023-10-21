07c19ee92659ccf379dc4d8655ce33f61b6e41b8
TomiHuspenina <127957400+TomiHuspenina@users.noreply.github.com>
2023-10-21 19:01:46 -0300

    Add files via upload

--- trabajofinal_Ec-Ar/main.cpp
+++ trabajofinal_Ec-Ar/main.cpp
@@ -2,34 +2,92 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include<set>
 #define Archivo "../Inventariado Fisico.csv"
 #include "HashMap/HashMap.h"
 #include "Lista/Lista.h"
+#include "Arbol/ArbolBinario.h"
 using namespace std;
 
-int main() {
+
+int total_art_dif(int& cant_art_dif){
+    cant_art_dif=0;
+    ifstream archivo(Archivo);
+    char delimitador = ',';
+    string linea;
+    std::set<std::string> grupos;
+
+    getline(archivo, linea); //ignorar el encabezado
+
+    while(getline(archivo, linea)){
+
+        std::stringstream lineStream(linea);
+        std::string grupo;
+
+        getline(lineStream, grupo, delimitador);
+        grupos.insert(grupo);
+
+    }
+
+    archivo.close();
+
+    cant_art_dif=grupos.size();
+
+    return cant_art_dif-1;
+}
+
+int total_art(int& cant_total){
+
+    ifstream archivo(Archivo);
+    string linea;
+    cant_total=0;
+
+    getline(archivo, linea); //ignorar el encabezado
+
+    while(getline(archivo, linea)){
+        cant_total++;
+    }
+
+    return cant_total-1;
+}
+
+
+int cantDepositos(int& depositos) {
+    depositos=0;
+    wchar_t delimiter = L',';
+    std::wifstream archivo(L"../Inventariado Fisico.csv");
+
+    if (!archivo.is_open()) {
+        std::cerr << "Error al abrir el archivo." << std::endl;
+        return -1;
+    }
+
+    archivo.imbue(std::locale(""));
+
+    std::wstring linea;
+
+    if (std::getline(archivo, linea)) {
+        std::wstringstream headerStream(linea);
+        std::wstring header;
+        while (std::getline(headerStream, header, delimiter)) {
+            if (header.find(L"deposito") != std::wstring::npos) {
+                depositos++;
+            }
+        }
+    }
+
+    return depositos;
+}
+
+
+void mostrarCVS(){
 
     ifstream archivo(Archivo);
     string linea;
     char delimitador = ',';
-    int n=0;
-
-    Lista<int> *L1 = new Lista<int>;
-    Lista<int> *L2 = new Lista<int>;
-    Lista<int> *L3 = new Lista<int>;
-    Lista<int> *L4 = new Lista<int>;
-    Lista<int> *L5 = new Lista<int>;
-    int i=0;
-
-
-
-    clock_t begin;
-    cout << "Comenzando a medir Tiempo\n" << endl;
-    begin = clock();
 
     if (!archivo.is_open()) {
         cerr << "Error al abrir el archivo." << endl;
-        return 1;
     }
 
     getline(archivo, linea);
@@ -37,37 +95,38 @@ int main() {
     while(getline(archivo, linea)){
 
         stringstream stream(linea);
-        string grupo, codigo_barras, articulo, dep1, dep2, dep3, dep4, dep5;
+        string grupo, codigo_barras, articulo;
 
         getline(stream, grupo, delimitador);
-        getline(stream, articulo, delimitador);
         getline(stream, codigo_barras, delimitador);
-        getline(stream, dep1, delimitador);
-        getline(stream, dep2, delimitador);
-        getline(stream, dep3, delimitador);
-        getline(stream, dep4, delimitador);
-        getline(stream, dep5, delimitador);
+        getline(stream, articulo, delimitador);
 
         cout << "==================" << endl;
         cout << "grupo: " << grupo << endl;
         cout << "articulo: " << articulo << endl;
         cout << "codigo de barras: " << codigo_barras << endl;
-        cout << "deposito 1: " << dep1 << endl;
-        cout << "deposito 2: " << dep2 << endl;
-        cout << "deposito 3: " << dep3 << endl;
-        cout << "deposito 4: " << dep4 << endl;
-        cout << "deposito 5: " << dep5 << endl;
-
-        n++;
-        
     }
-
-    cout<<"cantidad de archivos: "<<n<<endl;
 
     archivo.close();
 
+}
 
 
+int main() {
+
+    int depositos, n, m;
+
+    clock_t begin;
+    cout << "Comenzando a medir Tiempo\n" << endl;
+    begin = clock();
+
+    mostrarCVS();
+
+    cout<<"cantidad de articulos: "<<total_art(n)<<endl;
+
+    cout<<"cantidad de articulos diferentes: "<<total_art_dif(m)<<endl;
+
+    cout<<"cantidad de depositos: "<<cantDepositos(depositos)<<endl;
 
 
 
@@ -75,6 +134,6 @@ int main() {
 
     double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
     cout<<" "<<endl;
-    cout << "Tardo elapsed_secs" << elapsed_secs << "\n" << std::endl;
+    cout << "Tardo elapsed_secs  " << elapsed_secs << "\n" << std::endl;
     return 0;
 }
